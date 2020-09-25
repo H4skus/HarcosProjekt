c6cef7233220167703ede0ef84e0983d3f74af13
Ramirez Blényesi <haskushun@gmail.com>
2020-09-25 11:20:01 +0200

    Class(improved2)

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -5,9 +5,21 @@ namespace HarcosProjekt
 {
     class Program
     {
+        public static string name;
+
         static void Main(string[] args)
         {
-           
+            Start();
+        }
+
+
+        public static void Start()
+        {
+            Console.WriteLine("Kerem adja meg a nevet");
+            name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Kerem valasszon opciot! ");
+
         }
     }
 }
