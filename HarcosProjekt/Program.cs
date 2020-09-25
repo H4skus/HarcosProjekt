2eb2c4547533b19ae127359847e94f5f2a34b586
Ramirez Blényesi <haskushun@gmail.com>
2020-09-25 12:23:59 +0200

    Interface (Start) Formating

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -27,7 +27,11 @@ namespace HarcosProjekt
                 Console.WriteLine("\t\t\t\t\t\t    Welcome " + name + "\n\n\t\t\t\t\t\tChoose your Champ! \n\n\t\t\t\t(1)Warrior: \t Health: 15 \t Damage: 3 \n\t\t\t\t(2)Archer: \t Health: 12 \t Damage: 4 \n\t\t\t\t(3)Mage: \t Health: 8 \t Damage: 5");
                 if (!isNumber)
                 {
-                    Console.WriteLine("\n\t\t\t\t\t\tEz nem szam ");
+                    Console.WriteLine("\n\t\t\t\t\t\tIs not a number! ");
+                }
+                else if(chosenChamp < 1 || chosenChamp > 3)
+                {
+                    Console.WriteLine("\n\t\t\t\t\tHave no champion with this number! ");
                 }
                 isNumber =Int32.TryParse(Console.ReadLine(), out chosenChamp);
             }
