7ec2e78cac54e42f920a5e6d9b8028c39fc57ae3
Ramirez Blényesi <haskushun@gmail.com>
2020-09-25 10:29:01 +0200

    Create Class

---
+++ HarcosProjekt/Harcos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarcosProjekt
+{
+    class Harcos
+    {
+
+        private string name;
+        private int level, exp, health, startHealth, startDamage;
+
+        public Harcos(string name , int level, int exp, int health, int startHealth, int startDamage)
+        {
+            this.name = name;
+            this.level = level;
+            this.exp = exp;
+            this.health = health;
+            this.startHealth = startHealth;
+            this.startDamage = startDamage;
+        }
+    }
+}
